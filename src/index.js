88a7802be85b51a87a2bd551f45ed98f0cf3174e
Anton S <anton@sokolovski.com>
2020-08-21 11:48:47 +0300

    Создано динамическое изменение контента сайта, оптимизация кода в index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import {model} from './model.js'
+import {templates} from './templates.js' // экспорт функций
 import './styles/main.css'
+
+const site = document.querySelector('#site')
+model.forEach( block => {
+    const generate = templates[block.type] // генерация функций по типу
+
+    if (generate) {
+        const html = generate(block)
+        site.insertAdjacentHTML('beforeend', html)
+    }
+})
+
+
---
+++ src/model.js
@@ -0,0 +1,10 @@
+export const model = [
+    {type: 'title', value: 'Test title', options: {}},
+    {type: 'text', value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quos facere architecto perferendis natus nulla, excepturi voluptatem obcaecati adipisci', options: {}},
+    {type: 'textColumns', value: [
+        '1 text',
+        '2 text',
+        '3 text',
+        '4 text'
+    ], options: {}}
+]
